7573540b8928bead83c3ac20ede339e8ec7aecfe
binghannam <73022278+binghannam@users.noreply.github.com>
2020-12-09 08:46:16 +0400

    BombDropRandomiser

---
+++ Assets/Scripts/bombDropDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombDropDestroy : MonoBehaviour
+{
+    CameraShake cSh;
+    Rigidbody2D rb2dB;
+    public float dropForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2dB = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        cSh.ShakeCam();
+        Destroy(this.gameObject);
+        
+
+    }
+}
---
+++ Assets/Scripts/randomBombSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomBombSpawner : MonoBehaviour
+{
+
+    public GameObject theBomb;
+    public int xPos;
+    public int zPos;
+    public int bombCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        StartCoroutine(BombDrop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator BombDrop()
+    {
+
+        while (bombCount < 30)
+        {
+            xPos = Random.Range(-7, 7);
+            
+            Instantiate(theBomb, new Vector3(xPos, 4, 0), Quaternion.identity);
+            yield return new WaitForSeconds(0.3f);
+            bombCount += 1;
+        }
+    }
+
+
+}
